af02bdd61791339b5224f96e6adaad0562188bac
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-03-13 05:14:47 +0000

    Function that prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in matrix[i]:
+            if matrix[i].index(j) == 2:
+                print("{}".format(j), end="")
+            else:
+                print("{}".format(j), end=" ")
+        print("")
+
+
+if __name__ == "__main__":
+    print_matrix_integer()
