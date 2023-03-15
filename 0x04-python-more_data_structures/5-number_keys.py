d45f74d6b2a8dcfa6b622a2ded183c81464aa715
APIYOJENNIFER <apiyojennifer4@gmail.com>
2023-03-15 23:55:17 +0000

    Fix error if set is empty

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for i in enumerate(a_dictionary):
-        x, y = i
-    return x + 1
+    num = 0
+    if a_dictionary is not None:
+        for i in enumerate(a_dictionary):
+            x, y = i
+            num = x + 1
+        return num
 
 
 if __name__ == "__main__":
